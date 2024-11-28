160bf5730ba172d0599775d500444220270ede19
Sanjaikumaran <sanjaikumaran0311@gmail.com>
2024-11-28 23:31:55 +0530

    Demo Video Uploaded

--- Frontend/src/Admin/Anavbar.jsx
+++ Frontend/src/Admin/Anavbar.jsx
@@ -1,24 +1,76 @@
 // src/components/Navbar.js
 
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import {Link } from "react-router-dom"
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Anavbar = () => {
-  const get=localStorage.getItem('user')
+  const get = localStorage.getItem("user");
   return (
-    <Navbar bg="" variant="dark" expand="lg" style={{backgroundColor:"blue"}}>
+    <Navbar
+      bg=""
+      variant="dark"
+      expand="lg"
+      style={{ backgroundColor: "blue" }}
+    >
       <Container>
-        <Navbar.Brand><Link to='/shome' style={{color:"white",textDecoration:"none"}}>BookStore(Admin)</Link></Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/shome" style={{ color: "white", textDecoration: "none" }}>
+            BookStore(Admin)
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto" >
-            <Link to="/ahome" style={{padding:"10px",color:"white",textDecoration:"none"}}>Home</Link>
-            <Link to="/users" style={{padding:"10px",color:"white",textDecoration:"none"}}>Users</Link>
-            <Link to="/sellers" style={{padding:"10px",color:"white",textDecoration:"none"}}>Sellers</Link>
-            <Link to="/" style={{padding:"10px",color:"white",textDecoration:"none"}}>Logout</Link>
-            <h4 style={{color:"white",paddingTop:"0px"}}>({JSON.parse(get).name} )</h4>
-            
+          <Nav className="ml-auto">
+            <Link
+              to="/ahome"
+              style={{
+                padding: "10px",
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              Home
+            </Link>
+            <Link
+              to="/users"
+              style={{
+                padding: "10px",
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              Users
+            </Link>
+            <Link
+              to="/sellers"
+              style={{
+                padding: "10px",
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              Sellers
+            </Link>
+            <Link
+              to="/"
+              style={{
+                padding: "10px",
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              Logout
+            </Link>
+            <h4
+              style={{
+                color: "white",
+                paddingTop: "0px",
+                margin: "auto 10px ",
+              }}
+            >
+              ({JSON.parse(get).name} )
+            </h4>
           </Nav>
         </Navbar.Collapse>
       </Container>
